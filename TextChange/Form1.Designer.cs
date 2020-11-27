d86adb7bbb8c78aa89c30535a9424e9662839e22
Kirill <blinovk955@gmail.com>
2020-11-27 08:57:23 +0300

    Добавлена иконка

--- TextChange/Form1.Designer.cs
+++ TextChange/Form1.Designer.cs
@@ -237,6 +237,7 @@ namespace TextChange
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.groupBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.Name = "Form1";
 			this.Opacity = 0D;
